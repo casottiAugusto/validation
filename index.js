6e72e4b3676fb06cabff68e76b0be985d4b46ca9
Joao PC Windos <augusto_casotti@hotmail.com>
2022-08-24 20:33:13 -0300

    Base projeto

---
+++ index.js
@@ -0,0 +1,31 @@
+const bodyParser = require("body-parser");
+const express =require ("express");
+const session = require("express-session");
+const flash=require("express-flash");
+const app =express();
+
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+app.use(flash());
+
+
+
+
+
+app.get('/',(req,res)=>{
+  res.send("Teste")
+})
+
+
+
+app.listen(8080,(res,req)=>{
+console.log("To vivo!!!!!!!!!!!!")
+})
